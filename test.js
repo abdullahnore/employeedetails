b2b9294223c512cc3dd5aac463cea452b575348b
abdullah nore <49518940+abdullahnore@users.noreply.github.com>
2023-01-18 15:46:29 +0530

    cm

---
+++ test.js
@@ -0,0 +1,24 @@
+// // const fetch = import("node-fetch");
+// // const getdata = async () => {
+// //   const url = "http://localhost:5000/display";
+// //   const res = await fetch(url, {
+// //     method: "get",
+// //   });
+// //   console.log(res);
+// //   let data = res.JSON();
+// //   console.log(data);
+// // };
+
+// // getdata();
+// const { displaydata } = require("./file");
+// let display = [];
+// display = displaydata();
+// addEventListener;
+
+// console.log(display);
+
+// var str = new Headers();
+// str.append("accept", "name");
+// fetch("http://localhost:5000/display_data?id=name")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
